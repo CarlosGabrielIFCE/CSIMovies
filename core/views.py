b4470001fb1c728354c00a0c2389da668a3b6b5e
Carlos <carlos-cms2009@hotmail.com>
2018-02-17 19:05:46 -0300

    Deploy Final do projeto

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from django.shortcuts import render
 from .models import Film, Serie
 from .serializers import FilmSerializer
-from .forms import Contact
+
 
 class FilmList(generics.ListCreateAPIView):
     def get(self, request):
@@ -10,9 +10,15 @@ class FilmList(generics.ListCreateAPIView):
         films = Film.objects.all()
         serie = Serie.objects.get(pk=3)
         film = Film.objects.get(pk=17)
+        film2 = Film.objects.get(pk=18)
+        film3 = Film.objects.get(pk=19)
+        film4 = Film.objects.get(pk=20)
         serializer_class = FilmSerializer
         context = {
             'film': film,
+            'film2': film2,
+            'film3': film3,
+            'film4': film4,
             'serie': serie,
             'films': films,
             'series': series,
